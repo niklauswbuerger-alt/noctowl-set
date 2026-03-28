ddcc901fa656627fb710bb2294751776ab7389e2
niklauswbuerger-alt <niklauswbuerger@gmail.com>
2026-03-27 22:15:39 -0700

    Add files via upload

---
+++ noctowl-pwa-v3-fixed/sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'noctowl-v3';
+const CORE = [
+  '/noctowl-set/',
+  '/noctowl-set/index.html',
+  '/noctowl-set/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const net = fetch(e.request).then(res => {
+        if(res.ok){
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || net;
+    })
+  );
+});
